987716ee955b62da284fd45423d834443d1527ec
Karina <105536477+KarinaShupik@users.noreply.github.com>
2022-09-22 15:07:57 +0300

    Update Point.h

--- Point.h
+++ Point.h
@@ -2,6 +2,16 @@
 #include<iostream>
 #include<string>
 
+//Карина Шупик
+
+//202ТН
+
+/*Створити клас Point для роботи із точками на площині.Координати точки —
+декартові.Обов'язково повинні бути реалізовані: переміщення точки по осі X,
+переміщення по осі Y, визначення відстані до початку координат, відстані між
+двома точками, перетворення в полярні координати, порівняння на збіг і
+розбіжність.*/
+
 class Point
 {
 public:
@@ -12,9 +22,28 @@ public:
 	void Display();
 	char toString();
 
+	//конструктори
+	Point();
+	Point(float valueX, float valueY);
+
+	//перевантаження операцій порівняння, інкременту та декременту
+
+	//префіксний інкремент
+	Point& operator++ ();
+
+	//префіксний декремент
+	Point& operator-- ();
+
+	//збіжність
+	bool operator== (const Point& other);
+
+	//розбіжність
+	bool operator!= (const Point& other);
+
 private:
 	float distanceToStartCoordinate();
 	float polarCoordinates();
 	float transferX();
 	float transferY();
 };
+
